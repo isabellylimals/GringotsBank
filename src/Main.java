5f8b58502c061683f0ce7d02f02aa2a194c99b37
Isabelly Lima <mslima5643@gmail.com>
2025-02-23 18:07:06 -0300

    up

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
+        Menu menu = new Menu();
+        menu.menu();
         sc.close();
     }
 }
